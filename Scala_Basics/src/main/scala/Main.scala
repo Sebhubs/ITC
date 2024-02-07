ca9c8cfd11bbcd9de61907fbbff99f8f22ee2143
Sebhubs <se.boudaour@gmail.com>
2024-02-07 10:35:04 +0000

    This is a commit from IntelliJ

---
+++ Scala_Basics/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world 0 ")
+    println("Hello world 1 ")
+    println("Hello world 2 ")
+    println("Hello world 3 ")
+  }
+}
